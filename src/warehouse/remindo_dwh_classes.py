dca5caae30e1f436a9bda43cdf721e93c89fb7cb
Leonardo Vida <lleonardovida@gmail.com>
2020-10-20 12:06:58 +0200

    Align naming with remindo API

--- src/warehouse/remindo_dwh_classes.py
+++ src/warehouse/remindo_dwh_classes.py
@@ -2,9 +2,9 @@
 from sqlalchemy import Date, Float, String, Integer, Boolean, Column
 from sqlalchemy import ForeignKey, Text
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import Sequence
 
-#from src.warehouse.remindo_dwh_base import Base
+# from sqlalchemy.schema import Sequence
+# from src.warehouse.remindo_dwh_base import Base
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,12 +14,11 @@ Base = declarative_base()
 """
 TODO: Improve the naming of the arguments in the API and then transfer
 them here
-"""
-
-"""
-TODO: in the future add `id`, `load_date` and `job_run_id` 
+TODO: improve the name of the tables from API
+TODO: in the future add `id`, `load_date` and `job_run_id`
 to Base by creating a CommonBase as follows:
 Base = declarative_base(cls=CommonBase)
+TODO: Add the job_run_id automatically? With a sequence?
 """
 
 
@@ -29,7 +28,7 @@ class Cluster(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(200))
-    load_date = Column(Date)
+    extract_date = Column(Date)
     job_run_id = Column(Integer)
 
     def __repr__(self):
@@ -37,7 +36,7 @@ class Cluster(Base):
             load_date='%s', job_run_id='%s')>" % (
             self.id,
             self.name,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
@@ -54,24 +53,34 @@ class Study(Base):
     edition_descr = Column(String(200))
     source_edition_id = Column(Integer)
     source_study_id = Column(Integer)
-    # cluster_id = Column(Integer, foreign_key=True, nullable=False)
     apicall_complete = Column(Boolean)
     apicall_since = Column(Date)
 
-    recipes = relationship("Recipe", back_populates='study', cascade="all, delete")
-    moments = relationship("Moment", back_populates='study', cascade="all, delete")
+    # cluster_id = Column(Integer, foreign_key=True, nullable=False)
+    recipes = relationship(
+        "Recipe",
+        back_populates='study',
+        cascade="all, delete")
+    moments = relationship(
+        "Moment",
+        back_populates='study',
+        cascade="all, delete")
+    moments_results = relationship(
+        "Moment",
+        back_populates='study',
+        cascade="all, delete")
 
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Study(id='%s', name='%s', \
-            code='%s', record_create_timestamp='%s')>" % (
+            code='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.name,
             self.code,
-            self.record_create_timestamp
-            # self.job_run_id
+            self.extract_date,
+            self.job_run_id
         )
 
 
@@ -88,6 +97,8 @@ class Recipe(Base):
     max_retries = Column(Integer)
     exam_duration = Column(Integer)
     tools = Column(String(100))
+
+    # TODO: These are to test out
     practice_repeat_until = Column(Integer)
     practice_continue_practice = Column(String(50))
     practice_start_retry_by_candidate = Column(String(50))
@@ -103,37 +114,45 @@ class Recipe(Base):
     exam_round_grade_decimals = Column(String(10))
     bonuspoints = Column(Boolean)
     extra_time = Column(Boolean)
-    apicall_since = Column(Float)
 
-    # If null there are problems
+    # TODO: If null there are problems, but there shoulnd't be
+    apicall_since = Column(Float)
     apicall_study_id = Column(String(50))
     apicall_full = Column(String(10))
-
-    # To delete
     apicall_recipe_id = Column(Integer)
-    apicall_study_name = Column(String(200))
 
     study = relationship("Study", back_populates='recipes')
-    moments = relationship("Moment", back_populates='recipe', cascade="all, delete")
-    moments_results = relationship("MomentResult", back_populates='recipe', cascade="all, delete")
-    reliabilities = relationship("Reliability", back_populates='recipe', cascade="all, delete")
+    study_id = Column(Integer, ForeignKey(Study.id), ondelete="CASCADE")
+    # TODO: test out study_id = Column(Integer, ForeignKey('studies.id'))
+
+    moments = relationship(
+        "Moment",
+        back_populates='recipe',
+        cascade="all, delete")
+
+    moments_results = relationship(
+        "MomentResult",
+        back_populates='recipe',
+        cascade="all, delete")
+
+    reliabilities = relationship(
+        "Reliability",
+        back_populates='recipe',
+        cascade="all, delete")
 
     stats = relationship("Stat", cascade="all, delete")
     items = relationship("Item", cascade="all, delete")
 
-    # Do I have to do something with this?
-    study_id = Column(Integer, ForeignKey(Study.id))
-
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Recipe(id='%s', name='%s', \
-            code='%s', load_date='%s', job_run_id='%s')>" % (
+            code='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.name,
             self.code,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
@@ -146,6 +165,7 @@ class Moment(Base):
     name = Column(String(200))
     caesura = Column(String(1000))
     code = Column(String(200))
+    study_name = Column(String(100))
     datasource_id = Column(Integer)
     date_end = Column(String(50))
     date_start = Column(String(50))
@@ -168,7 +188,11 @@ class Moment(Base):
     apicall_recipe_id = Column(Integer)
 
     study = relationship("Study", back_populates='moments')
+    study_id = Column(Integer, ForeignKey(Study.id), ondelete="CASCADE")
+
     recipe = relationship("Recipe", back_populates='moments')
+    recipe_id = Column(Integer, ForeignKey(Recipe.id), ondelete="CASCADE")
+
     moment_results = relationship(
         "MomentResult", uselist=False, back_populates='moment', cascade="all, delete")
     reliability = relationship(
@@ -176,20 +200,16 @@ class Moment(Base):
     stats = relationship("Stat", cascade="all, delete")
     items = relationship("Item", cascade="all, delete")
 
-    study_id = Column(Integer, ForeignKey(Study.id))
-    study_name = Column(String(100))
-    recipe_id = Column(Integer, ForeignKey(Recipe.id))
-
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Moment(id='%s', name='%s', \
-            code='%s', load_date='%s', job_run_id='%s')>" % (
+            code='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.name,
             self.code,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
@@ -198,7 +218,8 @@ class MomentResult(Base):
     __tablename__ = 'moments_results'
     __table_args__ = {'schema': 'staging_schema'}
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    # TODO: id might be result_id
+    result_id = Column(Integer, primary_key=True, nullable=False)
     subscription_id = Column(Integer, nullable=False)
     user_id = Column(Integer)
     user_code = Column(String(50))
@@ -229,33 +250,35 @@ class MomentResult(Base):
     score_type = Column(String(50))
     grade_formatted = Column(Boolean)
     can_change = Column(Boolean)
-    apicall_recipe_id = Column(Integer)
-    apicall_moment_id = Column(Integer)
+
+    study = relationship("Study", back_populates='moments_results')
+    study_id = Column(Integer, ForeignKey(Study.id), ondelete="CASCADE")
 
     recipe = relationship("Recipe", back_populates='moments_results')
+    recipe_id = Column(Integer, ForeignKey(Recipe.id), ondelete="CASCADE")
+
     moment = relationship("Moment", back_populates='moment_results')
+    moment_id = Column(Integer, ForeignKey(Moment.id), ondelete="CASCADE")
 
-    recipe_id = Column(Integer, ForeignKey(Recipe.id))
-    moment_id = Column(Integer, ForeignKey(Moment.id))
-
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Moment(id='%s', moment_id='%s', \
-            recipe_id='%s', load_date='%s', job_run_id='%s')>" % (
+            recipe_id='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.moment_id,
             self.recipe_id,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
 
 class Reliability(Base):
-    __tablename__ = 'reliability'
+    __tablename__ = 'reliabilities'
     __table_args__ = {'schema': 'staging_schema'}
 
+    # TODO: sequence on Oracles
     id = Column(Integer, primary_key=True, nullable=False)
     alpha = Column(Float)
     sem = Column(Float)
@@ -265,31 +288,29 @@ class Reliability(Base):
     stdev = Column(Float)
     average = Column(Float)
     max = Column(Integer)
-    apicall_recipe_id = Column(Integer)
-    apicall_moment_id = Column(Integer)
 
     recipe = relationship(
         "Recipe",
         back_populates='reliabilities',
         cascade="all, delete")
+    recipe_id = Column(Integer, ForeignKey(Recipe.id))
+
     moment = relationship(
         "Moment",
         back_populates='reliability',
         cascade="all, delete")
-
-    recipe_id = Column(Integer, ForeignKey(Recipe.id))
     moment_id = Column(Integer, ForeignKey(Moment.id))
 
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(String)
 
     def __repr__(self):
         return "<Moment(id='%s', recipe_id='%s', \
-            moment_id='%s', load_date='%s', job_run_id='%s')>" % (
+            moment_id='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.recipe_id,
             self.moment_id,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
@@ -298,6 +319,7 @@ class Stat(Base):
     __tablename__ = 'stats'
     __table_args__ = {'schema': 'staging_schema'}
 
+    # TODO: sequence on Oracle
     id = Column(Integer, primary_key=True, nullable=False)
     item_identifier = Column(String(200), nullable=False)
     code = Column(String(200))
@@ -314,20 +336,20 @@ class Stat(Base):
     total = Column(Integer)
     answered = Column(Integer)
 
-    apicall_recipe_id = Column(Integer, ForeignKey(Recipe.id))
-    apicall_moment_id = Column(Integer, ForeignKey(Moment.id))
+    recipe_id = Column(Integer, ForeignKey(Recipe.id))
+    moment_id = Column(Integer, ForeignKey(Moment.id))
 
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Moment( \
             id='%s', recipe_id='%s', moment_id='%s', \
-                load_date='%s', job_run_id='%s')>" % (
+                extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.recipe_id,
             self.moment_id,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
@@ -336,6 +358,7 @@ class Item(Base):
     __tablename__ = 'items'
     __table_args__ = {'schema': 'staging_schema'}
 
+    # TODO: sequence on Oracle
     id = Column(Integer, primary_key=True, nullable=False)
     item_identifier = Column(String(200), nullable=False)
     num_attempts = Column(Integer)
@@ -347,7 +370,7 @@ class Item(Base):
     flagged = Column(Boolean)
     check_manually = Column(Boolean)
     weight = Column(Integer)
-    subscription_id = Column(Integer, nullable=False)
+    subscription_id = Column(Integer)
     position_item = Column(Integer)
     response_cardinality = Column(String(20))
     response_baseType = Column(String(20))
@@ -355,23 +378,20 @@ class Item(Base):
     response_candidateResponse = Column(Text)
     response_correctResponse = Column(Text)
 
-    apicall_recipe_id = Column(Integer)
-    apicall_moment_id = Column(Integer)
-
     recipe_id = Column(Integer, ForeignKey(Recipe.id))
     moment_id = Column(Integer, ForeignKey(Moment.id))
 
-    record_create_timestamp = Column(Date, nullable=False)
+    extract_date = Column(Date, nullable=False)
     job_run_id = Column(Integer)
 
     def __repr__(self):
         return "<Moment(id='%s', item_identifier='%s', moment_id='%s', \
-            recipe_id='%s', load_date='%s', job_run_id='%s')>" % (
+            recipe_id='%s', extract_date='%s', job_run_id='%s')>" % (
             self.id,
             self.item_identifier,
             self.moment_id,
             self.recipe_id,
-            self.load_date,
+            self.extract_date,
             self.job_run_id
         )
 
